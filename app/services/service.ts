0045a1ebbd0fe73558f4b453921d422c2a0285d1
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-06-29 07:31:50 +0800

    feat(service): add default properties to service class

--- app/services/service.ts
+++ app/services/service.ts
@@ -1,1 +1,20 @@
-export class Service {}
+export class Service {
+  page: number = 1;
+  pageCount: number = 15;
+
+  setPage(page: number): void {
+    this.page = page;
+  }
+
+  getPage(): number {
+    return this.page;
+  }
+
+  setPageCount(pageCount: number): void {
+    this.pageCount = pageCount;
+  }
+
+  getPageCount(): number {
+    return this.pageCount;
+  }
+}
