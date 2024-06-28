3834e2e0adfb73410ccf24595ff35aa2db66dade
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-06-29 06:02:16 +0800

    chore: add missing semicolons to code syntax

--- config/database.ts
+++ config/database.ts
@@ -1,5 +1,5 @@
-import app from '@adonisjs/core/services/app'
-import { defineConfig } from '@adonisjs/lucid'
+import app from '@adonisjs/core/services/app';
+import { defineConfig } from '@adonisjs/lucid';
 
 const dbConfig = defineConfig({
   connection: 'sqlite',
@@ -7,7 +7,7 @@ const dbConfig = defineConfig({
     sqlite: {
       client: 'better-sqlite3',
       connection: {
-        filename: app.tmpPath('db.sqlite3')
+        filename: app.tmpPath('db.sqlite3'),
       },
       useNullAsDefault: true,
       migrations: {
@@ -16,6 +16,6 @@ const dbConfig = defineConfig({
       },
     },
   },
-})
+});
 
-export default dbConfig
+export default dbConfig;
--- start/env.ts
+++ start/env.ts
@@ -9,7 +9,7 @@
 |
 */
 
-import { Env } from '@adonisjs/core/env'
+import { Env } from '@adonisjs/core/env';
 
 export default await Env.create(new URL('../', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
@@ -17,4 +17,4 @@ export default await Env.create(new URL('../', import.meta.url), {
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
-})
+});
