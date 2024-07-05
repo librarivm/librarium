f7d386ce8fbfffcad40133c2174cbf9728fd2e51
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-07-05 13:26:11 +0800

    fix(library): add table truncate on setup

--- tests/functional/api/v1/libraries/show.spec.ts
+++ tests/functional/api/v1/libraries/show.spec.ts
@@ -11,6 +11,7 @@ test.group(API_URL_NAME, (group) => {
   let $libraries: Library[] = [];
 
   group.setup(async () => {
+    await Library.truncate();
     $libraries = await LibraryFactory.with('user').with('type').createMany(10);
   });
 
