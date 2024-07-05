3a27867e364bf537d1ba3364e0a3f1c24a84a6e0
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-07-05 13:21:07 +0800

    feat: remove mock and replace with Test model

--- tests/mocks/models/mock_model.ts
+++ tests/mocks/models/mock_model.ts
@@ -1,95 +1,19 @@
-import isNil from 'lodash/isNil.js';
-import sinon from 'sinon';
+import { BaseModel, column } from '@adonisjs/lucid/orm';
+import { DateTime } from 'luxon';
 
-export const FakeModel = {
-  query: sinon.stub().returnsThis(),
-  apply: sinon.stub().returnsThis(),
-  where: sinon.stub().returnsThis(),
-  if: sinon.stub().returnsThis(),
+export default class Test extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number;
 
-  items: [],
-  attributes: {},
+  @column()
+  declare name: string;
 
-  mockAttributes: function (attributes: object | any) {
-    this.attributes = attributes;
+  @column()
+  declare description: string;
 
-    return this.attributes;
-  },
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime;
 
-  count: function () {
-    return this.items.length;
-  },
-
-  paginate: sinon.stub().callsFake((page: number, perPage: number) => {
-    const meta = {
-      total: 0,
-      perPage: perPage,
-      page: page,
-      lastPage: 1,
-    };
-    return {
-      rows: [],
-      forEach: () => [],
-      meta,
-      ...meta,
-    };
-  }),
-
-  find: sinon.stub().callsFake(function (id: number) {
-    // @ts-ignore
-    const item = this.items
-      .filter((attribute: any) => isNil(attribute.deleted_at))
-      .find((attribute: any) => attribute.id === id);
-
-    if (isNil(item)) {
-      return null;
-    }
-
-    // @ts-ignore
-    return Object.assign(this, { ...item });
-  }),
-
-  findOrFail: sinon.stub().callsFake(function (id: number) {
-    // @ts-ignore
-    const item = this.items.find((attribute: any) => attribute.id === id);
-
-    if (isNil(item)) {
-      throw new Error('Model Resource not found.');
-    }
-
-    // @ts-ignore
-    return Object.assign(this, { ...item });
-  }),
-
-  first: sinon.stub().callsFake(function () {
-    // @ts-ignore
-    return this.items?.[0];
-  }),
-
-  create: sinon.stub().callsFake(function (attributes: object) {
-    // @ts-ignore
-    const item = { ...attributes, id: this.count() + 1 };
-    // @ts-ignore
-    this.items = this.items.concat([item]);
-    return item;
-  }),
-
-  save: sinon.stub().callsFake(function () {
-    // @ts-ignore
-    Object.assign(this, { ...this.attributes, id: this.count() + 1 });
-
-    // @ts-ignore
-    return this;
-  }),
-
-  delete: sinon.stub().callsFake(function () {
-    // @ts-ignore
-    this.items = this.items.filter((item) => item.id !== this.id);
-
-    // @ts-ignore
-    Object.assign(this, { items: this.items });
-
-    // @ts-ignore
-    return this;
-  }),
-};
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime;
+}
