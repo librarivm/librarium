fda64e228cc54eee694eba43298f441bb9e162a2
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-07-09 00:44:51 +0800

    refactor: update `superadmin` description

--- app/enums/user_role.ts
+++ app/enums/user_role.ts
@@ -5,7 +5,7 @@ export enum UserRole {
 }
 
 export enum UserRoleDescription {
-  SUPERADMIN = 'somebody with full access to all application features',
+  SUPERADMIN = 'somebody with full access to all application features without restrictions',
   ADMIN = 'somebody who has access to all the administration features',
   SUBSCRIBER = 'somebody who can only manage their profile and view resources',
 }
