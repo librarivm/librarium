c767bafd5de4699da26ae99bfee951236a29c3be
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-07-09 01:23:20 +0800

    chore: temporarily comment out for CI

--- app/controllers/v1/roles_controller.ts
+++ app/controllers/v1/roles_controller.ts
@@ -16,20 +16,20 @@ export default class RolesController {
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  // async store({ request }: HttpContext) {}
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  // async show({ params }: HttpContext) {}
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  // async update({ params, request }: HttpContext) {}
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  // async destroy({ params }: HttpContext) {}
 }
