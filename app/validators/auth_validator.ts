bfa5e5442d8309efbc89f663c412c26ffdd36a99
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-07-15 08:16:52 +0800

    fix(auth): remove normalization of sub-addresses in emails

--- app/validators/auth_validator.ts
+++ app/validators/auth_validator.ts
@@ -5,12 +5,20 @@ const passwordSchema = vine.object({
   password: vine.string().minLength(8),
 });
 
+const emailNormalizationOptions = {
+  gmail_remove_dots: false,
+  gmail_remove_subaddress: false,
+  icloud_remove_subaddress: false,
+  outlookdotcom_remove_subaddress: false,
+  yahoo_remove_subaddress: false,
+};
+
 export const registerValidator = vine.compile(
   vine.object({
     email: vine
       .string()
       .email()
-      .normalizeEmail({ gmail_remove_dots: false, gmail_remove_subaddress: false })
+      .normalizeEmail(emailNormalizationOptions)
       .unique(async (db: Database, value: string): Promise<boolean> => {
         const matched = await db.from('users').select('id').where('email', value).first();
         return !matched;
@@ -21,10 +29,7 @@ export const registerValidator = vine.compile(
 
 export const loginValidator = vine.compile(
   vine.object({
-    email: vine
-      .string()
-      .email()
-      .normalizeEmail({ gmail_remove_dots: false, gmail_remove_subaddress: false }),
+    email: vine.string().email().normalizeEmail(emailNormalizationOptions),
     password: passwordSchema.getProperties().password,
   })
 );
