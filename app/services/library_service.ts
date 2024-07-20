a8452765617e79976c90dbb2f4e2fffec28346ec
John Lioneil Dionisio <john.dionisio1@gmail.com>
2024-07-21 04:00:51 +0800

    fix(library): update LibraryService supported columns and their endpoint documentation

--- app/services/library_service.ts
+++ app/services/library_service.ts
@@ -18,7 +18,14 @@ export type LibraryAttributes = {
 
 @inject()
 export default class LibraryService extends Service {
-  supportedColumnKeys: string[] = ['name', 'description', 'slug', 'created_at', 'updated_at'];
+  supportedColumnKeys: string[] = [
+    'name',
+    'description',
+    'slug',
+    'is_private',
+    'created_at',
+    'updated_at',
+  ];
 
   preloads: string[] = ['user', 'type'];
 
